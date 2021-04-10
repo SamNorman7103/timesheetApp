305a209215b2ad3b28ba49041c872c382f01e1eb
SamNorman7103 <70028250+SamNorman7103@users.noreply.github.com>
2021-04-10 16:39:15 -0600

    u12 completed

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from 'src/app/services/departments.service';
 import { Department } from 'src/app/interfaces/department'
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-departments',
@@ -9,18 +10,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./departments.component.scss']
 })
 export class DepartmentsComponent implements OnInit {
-  departments: Department[];
-
+  
+  $departments: Observable<Department[]>;
   constructor(
     private departmentsService: DepartmentsService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.departmentsService.getDeparments().subscribe(departments => {
-      this.departments = departments;
-  });
-  
+    this.$departments = this.departmentsService.getDeparments();
+
   }
 
   goToDepartment(departmentId: string): void {
