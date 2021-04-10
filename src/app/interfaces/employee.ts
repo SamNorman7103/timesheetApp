05298a5e9a68e7c72d6e8a505d0d3c22de189112
SamNorman7103 <70028250+SamNorman7103@users.noreply.github.com>
2021-04-10 12:45:46 -0600

    u8 pt 1 complete

--- src/app/interfaces/employee.ts
+++ src/app/interfaces/employee.ts
@@ -3,6 +3,13 @@ export interface Employee {
     departmentId: string;
     name: string;
     payRate: number;
+    monday: number;
+    tuesday: number;
+    wednesday: number;
+    thursday: number;
+    friday: number;
+    saturday: number;
+    sunday: number;
 }
 
 
